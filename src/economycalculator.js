6a6d718e7482a44fad4618f601bbebbc2516eae1
laptop <nolic0321@gmail.com>
2019-01-24 12:20:25 -0600

    Started working on routing.  Might want to use react-dom-route instead

---
+++ src/economycalculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class EconomyCalculator extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Test text</h1>
+                <br/>
+                Moar text
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { EconomyCalculator } from "economycalculator";
+import style from './style.css';
+
+render(<EconomyCalculator />, document.getElementById('root'));
