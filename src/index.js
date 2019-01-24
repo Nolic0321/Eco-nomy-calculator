bb435cc3e5944c61b613629ea2f46adb7c675a2a
Nolic0321 <nolic0321@gmail.com>
2019-01-24 15:36:57 -0600

    Updated to use react boilerplate

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { EconomyCalculator } from "economycalculator";
-import style from './style.css';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-render(<EconomyCalculator />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
